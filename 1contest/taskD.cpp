d212543a9cf0bba8e2bc5ce5a3e476c9fd30a410
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2022-10-11 21:24:00 +0300

    commit taskD_rev1

--- 1contest/taskD.cpp
+++ 1contest/taskD.cpp
@@ -1,53 +1,55 @@
+
 #include <algorithm>
 #include <deque>
 #include <iostream>
 
-std::deque<int> fr, bck;
-
-void CheckAmount() {
-  while (fr.size() < bck.size()) {
-    int cur = bck.front();
-    bck.pop_front();
-    fr.push_back(cur);
+struct SuperQueue {
+  std::deque<int> fr, bck;
+  SuperQueue() {}
+  void CheckAmount() {
+    while (fr.size() < bck.size()) {
+      int cur = bck.front();
+      bck.pop_front();
+      fr.push_back(cur);
+    }
   }
-}
-
-void PushLast(int val) {
-  CheckAmount();
-  bck.push_back(val);
-  CheckAmount();
-}
-
-void PushMiddle(int val) {
-  CheckAmount();
-  if (fr.size() == bck.size()) {
-    fr.push_back(val);
-  } else {
-    bck.push_front(val);
+  void PushLast(int val) {
+    CheckAmount();
+    bck.push_back(val);
+    CheckAmount();
   }
-  CheckAmount();
-}
+  void PushMiddle(int val) {
+    CheckAmount();
+    if (fr.size() == bck.size()) {
+      fr.push_back(val);
+    } else {
+      bck.push_front(val);
+    }
+    CheckAmount();
+  }
+};
 
 int main() {
   std::cin.tie(0);
   std::cout.tie(0);
   int n;
   std::cin >> n;
+  SuperQueue curr_queue;
   for (int q = 0; q < n; ++q) {
     char v;
     std::cin >> v;
     if (v == '-') {
-      int ans = fr.front();
-      fr.pop_front();
-      CheckAmount();
+      int ans = curr_queue.fr.front();
+      curr_queue.fr.pop_front();
+      curr_queue.CheckAmount();
       std::cout << ans << "\n";
     } else {
       int num;
       std::cin >> num;
       if (v == '*') {
-        PushMiddle(num);
+        curr_queue.PushMiddle(num);
       } else {
-        PushLast(num);
+        curr_queue.PushLast(num);
       }
     }
   }
