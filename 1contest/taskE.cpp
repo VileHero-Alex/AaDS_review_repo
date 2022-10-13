5e615cfd6bbb10d417abf9bd93f0b2523a67bdde
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2022-10-13 20:09:34 +0300

    1contest taskE rev_1
    https://contest.yandex.ru/contest/39305/run-report/71990572/

--- 1contest/taskE.cpp
+++ 1contest/taskE.cpp
@@ -2,84 +2,103 @@
 #include <iostream>
 #include <string>
 
-int q;
-std::string t;
-int queue[(int)2e5 + 100];
-int mins[(int)2e5 + 100];
-int q_l = 0, q_r = 0, mn_l = 0, mn_r = 0;
+struct SuperQueue {
+  const int kSz = (int)(2e5) + 100;
 
-int Size() { return (q_r - q_l); }
+  int* queue;
+  int* mins;
+  int q_l, q_r, mn_l, mn_r;
 
-void Enqueue() {
-  int x;
-  std::cin >> x;
-  queue[q_r] = x;
-  q_r++;
-  while (mn_l < mn_r && mins[mn_r - 1] > x) {
-    mn_r--;
+  SuperQueue() {
+    q_l = 0;
+    q_r = 0;
+    mn_l = 0;
+    mn_r = 0;
+    queue = new int[kSz];
+    mins = new int[kSz];
   }
-  mins[mn_r] = x;
-  mn_r++;
-  std::cout << "ok\n";
-}
 
-void Dequeue() {
-  if (Size() == 0) {
-    std::cout << "error\n";
-    return;
+  ~SuperQueue() {
+    delete[] queue;
+    delete[] mins;
   }
-  if (queue[q_l] == mins[mn_l]) {
-    mn_l++;
-  }
-  std::cout << queue[q_l] << "\n";
-  q_l++;
-}
 
-void Front() {
-  if (Size() == 0) {
-    std::cout << "error\n";
-    return;
-  }
-  std::cout << queue[q_l] << "\n";
-}
+  int Size() const { return (q_r - q_l); }
 
-void Clear() {
-  q_l = q_r = 0;
-  mn_l = mn_r = 0;
-  std::cout << "ok\n";
-}
-
-void FindMin() {
-  if (Size() == 0) {
-    std::cout << "error\n";
-    return;
+  void Enqueue() {
+    int x;
+    std::cin >> x;
+    queue[q_r] = x;
+    ++q_r;
+    while (mn_l < mn_r && mins[mn_r - 1] > x) {
+      --mn_r;
+    }
+    mins[mn_r] = x;
+    ++mn_r;
+    std::cout << "ok\n";
   }
-  std::cout << mins[mn_l] << "\n";
-}
+
+  void Dequeue() {
+    if (Size() == 0) {
+      std::cout << "error\n";
+      return;
+    }
+    if (queue[q_l] == mins[mn_l]) {
+      ++mn_l;
+    }
+    std::cout << queue[q_l] << "\n";
+    ++q_l;
+  }
+
+  void Front() const {
+    if (Size() == 0) {
+      std::cout << "error\n";
+      return;
+    }
+    std::cout << queue[q_l] << "\n";
+  }
+
+  void Clear() {
+    q_l = q_r = 0;
+    mn_l = mn_r = 0;
+    std::cout << "ok\n";
+  }
+
+  void FindMin() const {
+    if (Size() == 0) {
+      std::cout << "error\n";
+      return;
+    }
+    std::cout << mins[mn_l] << "\n";
+  }
+};
 
 int main() {
   std::cin.tie(0);
   std::cout.tie(0);
+  SuperQueue curr_queue;
+  int q;
   std::cin >> q;
+  std::string t;
   for (int que = 0; que < q; ++que) {
     std::cin >> t;
     if (t == "enqueue") {
-      Enqueue();
+      curr_queue.Enqueue();
     }
     if (t == "dequeue") {
-      Dequeue();
+      curr_queue.Dequeue();
     }
     if (t == "front") {
-      Front();
+      curr_queue.Front();
     }
     if (t == "size") {
-      std::cout << Size() << "\n";
+      std::cout << curr_queue.Size() << "\n";
     }
     if (t == "clear") {
-      Clear();
+      curr_queue.Clear();
     }
     if (t == "min") {
-      FindMin();
+      curr_queue.FindMin();
     }
   }
 }
