16da3c295c9810c5801c07419817723d296fc068
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2022-11-14 12:26:03 +0300

    commit 2contest taskB rev
    https://contest.yandex.ru/contest/39313/run-report/74751598/

--- 2contest/taskB.cpp
+++ 2contest/taskB.cpp
@@ -8,14 +8,24 @@ const int kMod = static_cast<int>(1e7) + 4321;
 }
 
 int Rnd(int lf, int rt) {
-  std::mt19937 rnd(rand());
-  return lf + rnd() % (rt - lf);
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_int_distribution<> distrib(lf, rt);
+  return distrib(gen);
 }
 
 void Partition(int lf, int rt, int& md, int& pos, int* data) {
   md = data[Rnd(lf, rt)], pos = lf;
   for (int i = lf; i < rt; ++i) {
     if (data[i] < md) {
+      std::swap(data[i], data[pos++]);
+    }
+  }
+}
+
+void PostPartition(int& pos, int rt, int md, int* data) {
+  for (int i = pos; i < rt; ++i) {
+    if (data[i] == md) {
       std::swap(data[i], data[pos++]);
     }
   }
@@ -30,11 +40,7 @@ int KthSearch(int lf, int rt, int k, int* data) {
   if (k <= pos - lf) {
     return KthSearch(lf, pos, k, data);
   }
-  for (int i = pos; i < rt; i++) {
-    if (data[i] == md) {
-      std::swap(data[i], data[pos++]);
-    }
-  }
+  PostPartition(pos, rt, md, data);
   if (k <= pos - lf) {
     return data[pos - 1];
   }
@@ -47,20 +53,16 @@ void SpeedUp() {
   std::ios_base::sync_with_stdio(false);
 }
 
-void Init(int& n, int& kth, int*& data) {
+void Solve() {
+  int n, kth;
   std::cin >> n >> kth;
-  data = new int[n];
+  int* data = new int[n];
   std::cin >> data[0] >> data[1];
   for (int i = 2; i < n; ++i) {
     data[i] = (data[i - 1] * 123 + data[i - 2] * 45) % MyConst::kMod;
   }
-}
-
-void Solve() {
-  int n, kth;
-  int* data;
-  Init(n, kth, data);
   std::cout << KthSearch(0, n, kth, data);
+  delete[] data;
 }
 
 int main() {
