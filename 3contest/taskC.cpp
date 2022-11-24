aba171e1d35cee0f493897fa75bed1096f960e01
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2022-11-25 00:05:29 +0300

    commit contest3 taskC rev
    https://contest.yandex.ru/contest/39314/run-report/76152396/

--- 3contest/taskC.cpp
+++ 3contest/taskC.cpp
@@ -11,37 +11,6 @@ class SegmentTree {
  private:
   int* tree_;
 
- public:
-  const int kINF = static_cast<int>(1e9);
-
-  SegmentTree(int sz) { tree_ = new int[sz * 5]; }
-  ~SegmentTree() { delete[] tree_; }
-
-  void Build(int v, int l, int r, const std::vector<int>& a) {
-    if (l + 1 == r) {
-      tree_[v] = a[l];
-      return;
-    }
-    int md = (l + r) / 2;
-    Build(v * 2, l, md, a);
-    Build(v * 2 + 1, md, r, a);
-    tree_[v] = std::max(tree_[2 * v], tree_[2 * v + 1]);
-  }
-
-  void Update(int v, int l, int r, int pos, int val) {
-    if (l + 1 == r) {
-      tree_[v] = val;
-      return;
-    }
-    int md = (l + r) / 2;
-    if (pos < md) {
-      Update(2 * v, l, md, pos, val);
-    } else {
-      Update(2 * v + 1, md, r, pos, val);
-    }
-    tree_[v] = std::max(tree_[2 * v], tree_[2 * v + 1]);
-  }
-
   int GetRes(int v, int l, int r, int val) {
     if (tree_[v] < val) {
       return -kINF;
@@ -56,24 +25,15 @@ class SegmentTree {
     return GetRes(2 * v + 1, md, r, val);
   }
 
-  int GetCover(int v, int l, int r, std::pair<int, int> query, int val) {
-    int ql = query.first, qr = query.second;
-    if (ql == l && qr == r) {
-      return GetRes(v, l, r, val);
-    }
-    int md = (l + r) / 2;
-    if (ql < md) {
-      int res = GetCover(2 * v, l, md, {ql, std::min(qr, md)}, val);
-      if (res != -kINF) {
-        return res;
-      }
-    }
-    if (qr > md) {
-      int res = GetCover(2 * v + 1, md, r, {std::max(ql, md), qr}, val);
-      return res;
-    }
-    return -kINF;
-  }
+ public:
+  static const int kINF = static_cast<int>(1e9);
+
+  SegmentTree(int sz) { tree_ = new int[sz * 4]; }
+  ~SegmentTree() { delete[] tree_; }
+
+  void Build(int v, int l, int r, const std::vector<int>& a);
+  void Update(int v, int l, int r, int pos, int val);
+  int GetTopCover(int v, int l, int r, std::pair<int, int> query, int val);
 };
 
 void Solve() {
@@ -92,7 +52,7 @@ void Solve() {
     if (t == 0) {
       tree.Update(1, 0, n, pos, x);
     } else {
-      int ans = tree.GetCover(1, 0, n, {pos, n}, x);
+      int ans = tree.GetTopCover(1, 0, n, {pos, n}, x);
       if (ans == -tree.kINF) {
         std::cout << "-1\n";
       } else {
@@ -106,4 +66,49 @@ int main() {
   SpeedUp();
   Solve();
   return 0;
+}
+
+void SegmentTree::Build(int v, int l, int r, const std::vector<int>& a) {
+  if (l + 1 == r) {
+    tree_[v] = a[l];
+    return;
+  }
+  int md = (l + r) / 2;
+  Build(v * 2, l, md, a);
+  Build(v * 2 + 1, md, r, a);
+  tree_[v] = std::max(tree_[2 * v], tree_[2 * v + 1]);
+}
+
+void SegmentTree::Update(int v, int l, int r, int pos, int val) {
+  if (l + 1 == r) {
+    tree_[v] = val;
+    return;
+  }
+  int md = (l + r) / 2;
+  if (pos < md) {
+    Update(2 * v, l, md, pos, val);
+  } else {
+    Update(2 * v + 1, md, r, pos, val);
+  }
+  tree_[v] = std::max(tree_[2 * v], tree_[2 * v + 1]);
+}
+
+int SegmentTree::GetTopCover(int v, int l, int r, std::pair<int, int> query,
+                             int val) {
+  int ql = query.first, qr = query.second;
+  if (ql == l && qr == r) {
+    return GetRes(v, l, r, val);
+  }
+  int md = (l + r) / 2;
+  if (ql < md) {
+    int res = GetTopCover(2 * v, l, md, {ql, std::min(qr, md)}, val);
+    if (res != -kINF) {
+      return res;
+    }
+  }
+  if (qr > md) {
+    int res = GetTopCover(2 * v + 1, md, r, {std::max(ql, md), qr}, val);
+    return res;
+  }
+  return -kINF;
 }
