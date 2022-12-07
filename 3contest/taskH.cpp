7bc19e38990a4b11971e0a76a12bdcdc902049a2
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2022-12-07 03:02:49 +0300

    commit 3contest taskH rev
    https://contest.yandex.ru/contest/39314/run-report/78910635/

--- 3contest/taskH.cpp
+++ 3contest/taskH.cpp
@@ -2,76 +2,24 @@
 #include <iostream>
 #include <vector>
 
+struct MyElement {
+  int first_min, second_min;
+};
+
 class SparseTable {
  private:
-  const int kInf = static_cast<int>(1e9);
+  static const int kInf = static_cast<int>(1e9);
   std::vector<int> lg_, myCopy_;
-  std::vector<std::vector<std::pair<int, int>>> table_;
-  std::pair<int, int> GetValToMerge(std::pair<int, int> v1,
-                                    std::pair<int, int> v2) {
-    std::vector<int> temp = {v1.first, v1.second, v2.first, v2.second};
-    std::sort(temp.begin(), temp.end(), [&](int p1, int p2) {
-      if (p1 == kInf) {
-        if (p2 == kInf) {
-          return true;
-        }
-        return p1 < p2;
-      }
-      if (p2 == kInf) {
-        return p1 < p2;
-      }
-      return myCopy_[p1] < myCopy_[p2];
-    });
-    temp.resize(std::unique(temp.begin(), temp.end()) - temp.begin());
-    return {temp[0], temp[1]};
-  }
+  std::vector<std::vector<MyElement>> table_;
 
-  int GetAns(std::pair<int, int> v1, std::pair<int, int> v2) {
-    std::vector<int> temp = {v1.first, v1.second, v2.first, v2.second};
-    std::sort(temp.begin(), temp.end(), [&](int p1, int p2) {
-      if (p1 == kInf) {
-        if (p2 == kInf) {
-          return true;
-        }
-        return p1 < p2;
-      }
-      if (p2 == kInf) {
-        return p1 < p2;
-      }
-      return myCopy_[p1] < myCopy_[p2];
-    });
-    temp.resize(std::unique(temp.begin(), temp.end()) - temp.begin());
-    return temp[1];
-  }
+  MyElement GetValToMerge(MyElement v1, MyElement v2);
+
+  int GetAns(MyElement v1, MyElement v2);
 
  public:
-  SparseTable(int n, std::vector<int>& arr) {
-    lg_.assign(n + 1, 0);
-    myCopy_.assign(n, 0);
-    for (int i = 0; i < n; ++i) {
-      myCopy_[i] = arr[i];
-    }
-    for (int i = 2; i <= n; i++) {
-      lg_[i] = lg_[i / 2] + 1;
-    }
-    table_.assign(lg_[n] + 1,
-                  std::vector<std::pair<int, int>>(n, {kInf, kInf}));
-    for (int i = 0; i < n; ++i) {
-      table_[0][i] = {i, kInf};
-    }
-    for (int log_sz = 1; log_sz <= lg_[n]; ++log_sz) {
-      for (int i = 0; i + (1LL << log_sz) <= n; ++i) {
-        table_[log_sz][i] =
-            GetValToMerge(table_[log_sz - 1][i],
-                          table_[log_sz - 1][i + (1LL << (log_sz - 1))]);
-      }
-    }
-  }
+  SparseTable(std::vector<int>& arr);
 
-  int GetRes(int l, int r) {
-    int seg_sz = lg_[r - l + 1];
-    return GetAns(table_[seg_sz][l], table_[seg_sz][r - (1LL << seg_sz) + 1]);
-  }
+  int GetRes(int l, int r);
 };
 
 void SpeedUp() {
@@ -87,7 +35,7 @@ void Solve() {
   for (int i = 0; i < n; ++i) {
     std::cin >> a[i];
   }
-  SparseTable mns(n, a);
+  SparseTable minimums(a);
   for (int i = 0; i < q; ++i) {
     int l, r;
     std::cin >> l >> r;
@@ -95,7 +43,7 @@ void Solve() {
       std::swap(l, r);
     }
     --l, --r;
-    std::cout << a[mns.GetRes(l, r)] << "\n";
+    std::cout << a[minimums.GetRes(l, r)] << "\n";
   }
 }
 
@@ -103,4 +51,47 @@ signed main() {
   SpeedUp();
   Solve();
   return 0;
+}
+
+int SparseTable::GetAns(MyElement v1, MyElement v2) {
+  return GetValToMerge(v1, v2).second_min;
+}
+
+MyElement SparseTable::GetValToMerge(MyElement v1, MyElement v2) {
+  std::vector<int> temp = {v1.first_min, v1.second_min, v2.first_min,
+                           v2.second_min};
+  std::sort(temp.begin(), temp.end(), [this](int p1, int p2) {
+    if (p1 == kInf || p2 == kInf) {
+      return p1 < p2;
+    }
+    return myCopy_[p1] < myCopy_[p2];
+  });
+  temp.resize(std::unique(temp.begin(), temp.end()) - temp.begin());
+  return {temp[0], temp[1]};
+}
+
+SparseTable::SparseTable(std::vector<int>& arr) {
+  int n = static_cast<int>(arr.size());
+  lg_.assign(n + 1, 0);
+  myCopy_ = arr;
+  for (int i = 2; i <= n; i++) {
+    lg_[i] = lg_[i / 2] + 1;
+  }
+  table_.assign(lg_[n] + 1,
+                std::vector<MyElement>(
+                    n, {static_cast<int>(kInf), static_cast<int>(kInf)}));
+  for (int i = 0; i < n; ++i) {
+    table_[0][i] = {i, static_cast<int>(kInf)};
+  }
+  for (int log_sz = 1; log_sz <= lg_[n]; ++log_sz) {
+    for (int i = 0; i + (1LL << log_sz) <= n; ++i) {
+      table_[log_sz][i] = GetValToMerge(
+          table_[log_sz - 1][i], table_[log_sz - 1][i + (1LL << (log_sz - 1))]);
+    }
+  }
+}
+
+int SparseTable::GetRes(int l, int r) {
+  int seg_sz = lg_[r - l + 1];
+  return GetAns(table_[seg_sz][l], table_[seg_sz][r - (1LL << seg_sz) + 1]);
 }
