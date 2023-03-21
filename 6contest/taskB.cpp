9e4811cc3305a97ce542e01d581445c3e13990f1
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2023-03-21 17:43:21 +0300

    commit taskB contest6 v2

--- 6contest/taskB.cpp
+++ 6contest/taskB.cpp
@@ -4,11 +4,16 @@
 
 const int kInf = 2147483647;
 
-int BinarySearch(std::vector<int>& dp, int x) {
+struct AnsForPrefix {
+  int val = kInf;
+  int pos = -1;
+};
+
+int BinarySearch(const std::vector<AnsForPrefix>& dp, int x) {
   int l = 0, r = dp.size();
   while (l + 1 < r) {
     int m = (l + r) / 2;
-    if (dp[m] <= x) {
+    if (dp[m].val <= x) {
       l = m;
     } else {
       r = m;
@@ -17,38 +22,50 @@ int BinarySearch(std::vector<int>& dp, int x) {
   return l + 1;
 }
 
+std::vector<int> Solve(int n, std::vector<int> s) {
+  for (int i = 0; i < n; ++i) {
+    s[i] *= -1;  // умножаю минус на -1, так как найдётся -a<-b<...<-x - что
+                 // равносильно a>b>...>x - а это нам и нужно
+  }
+  std::vector<int> answer, parent(n, -1);
+  std::vector<AnsForPrefix> dp(n + 1);  // d[i] (i = 0...n) — это число, на
+                                        // которое оканчивается возрастающая
+                                        // подпоследовательность длины i
+                                        // (а если таких чисел несколько —
+                                        // то наименьшее из них).
+  dp[0].val = -kInf;
+  int len = 0;
+
+  for (int i = 0; i < n; ++i) {
+    int pos = BinarySearch(dp, s[i]);
+    if (dp[pos - 1].val <= s[i] && s[i] < dp[pos].val) {
+      dp[pos].val = s[i];
+      dp[pos].pos = i;
+      parent[i] = dp[pos - 1].pos;
+      len = std::max(len, pos);
+    }
+  }
+  int pos = dp[len].pos;
+  while (pos != -1) {
+    answer.push_back(pos);
+    pos = parent[pos];
+  }
+  return answer;
+}
+
 int main() {
   int n;
   std::cin >> n;
   std::vector<int> s(n);
   for (int i = 0; i < n; ++i) {
     std::cin >> s[i];
-    s[i] *= -1;
-  }
-  std::vector<int> dp(n + 1, kInf), dp_pos(n + 1, -1), parent(n, -1);
-  dp[0] = -kInf;
-  int len = 0;
-
-  for (int i = 0; i < n; ++i) {
-    int pos = BinarySearch(dp, s[i]);
-    if (dp[pos - 1] <= s[i] && s[i] < dp[pos]) {
-      dp[pos] = s[i];
-      dp_pos[pos] = i;
-      parent[i] = dp_pos[pos - 1];
-      len = std::max(len, pos);
-    }
   }
   std::vector<int> answer;
-  int pos = dp_pos[len];
-  while (pos != -1) {
-    answer.push_back(pos);
-    pos = parent[pos];
-  }
-  std::cout << len << "\n";
+  answer = Solve(n, s);
+  std::cout << answer.size() << "\n";
   std::reverse(answer.begin(), answer.end());
   for (auto x : answer) {
     std::cout << x + 1 << " ";
   }
-
   return 0;
 }
