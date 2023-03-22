8e0f2db4a74c433c4ad5357018a1c98f0c20844d
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2023-03-22 18:14:30 +0300

    commit taskB contest6 v3

--- 6contest/taskB.cpp
+++ 6contest/taskB.cpp
@@ -5,7 +5,7 @@
 const int kInf = 2147483647;
 
 struct AnsForPrefix {
-  int val = kInf;
+  int val = -kInf;
   int pos = -1;
 };
 
@@ -13,7 +13,7 @@ int BinarySearch(const std::vector<AnsForPrefix>& dp, int x) {
   int l = 0, r = dp.size();
   while (l + 1 < r) {
     int m = (l + r) / 2;
-    if (dp[m].val <= x) {
+    if (dp[m].val >= x) {
       l = m;
     } else {
       r = m;
@@ -22,23 +22,20 @@ int BinarySearch(const std::vector<AnsForPrefix>& dp, int x) {
   return l + 1;
 }
 
-std::vector<int> Solve(int n, std::vector<int> s) {
-  for (int i = 0; i < n; ++i) {
-    s[i] *= -1;  // умножаю минус на -1, так как найдётся -a<-b<...<-x - что
-                 // равносильно a>b>...>x - а это нам и нужно
-  }
+std::vector<int> FindTheLargestNonIncreasingSubsequence(int n,
+                                                        std::vector<int> s) {
   std::vector<int> answer, parent(n, -1);
   std::vector<AnsForPrefix> dp(n + 1);  // d[i] (i = 0...n) — это число, на
                                         // которое оканчивается возрастающая
                                         // подпоследовательность длины i
                                         // (а если таких чисел несколько —
                                         // то наименьшее из них).
-  dp[0].val = -kInf;
+  dp[0].val = kInf;
   int len = 0;
 
   for (int i = 0; i < n; ++i) {
     int pos = BinarySearch(dp, s[i]);
-    if (dp[pos - 1].val <= s[i] && s[i] < dp[pos].val) {
+    if (dp[pos - 1].val >= s[i] && s[i] > dp[pos].val) {
       dp[pos].val = s[i];
       dp[pos].pos = i;
       parent[i] = dp[pos - 1].pos;
@@ -50,6 +47,7 @@ std::vector<int> Solve(int n, std::vector<int> s) {
     answer.push_back(pos);
     pos = parent[pos];
   }
+  std::reverse(answer.begin(), answer.end());
   return answer;
 }
 
@@ -61,9 +59,8 @@ int main() {
     std::cin >> s[i];
   }
   std::vector<int> answer;
-  answer = Solve(n, s);
+  answer = FindTheLargestNonIncreasingSubsequence(n, s);
   std::cout << answer.size() << "\n";
-  std::reverse(answer.begin(), answer.end());
   for (auto x : answer) {
     std::cout << x + 1 << " ";
   }
