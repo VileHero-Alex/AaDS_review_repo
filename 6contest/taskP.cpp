a5ec9e5e0dc8626de536a94f1cab49a771bdc5a9
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2023-03-30 18:13:06 +0300

    commit taskP contest6 v2

--- 6contest/taskP.cpp
+++ 6contest/taskP.cpp
@@ -3,27 +3,27 @@
 #include <vector>
 
 const int kMod = 1'000'000'007;
+const int kUnused = -1;
 
-bool Check(int n, int mask, int pos, std::vector<std::string>& s) {
+bool Check(int n, int mask, int pos, const std::vector<std::string>& map) {
   for (int i = 0; i < n; ++i) {
-    if (s[i][pos] == '+' && ((mask >> i) & 1) == 0) {
+    if (map[i][pos] == '+' && ((mask >> i) & 1) == 0) {
       return false;
     }
-    if (s[i][pos] == '-' && ((mask >> i) & 1) != 0) {
+    if (map[i][pos] == '-' && ((mask >> i) & 1) != 0) {
       return false;
     }
   }
   return true;
 }
 
-void MakeCorresponing(int n, int mask, int& opp, bool& fl) {
+bool IsThereCorresponing(int n, int mask, int& opp) {
   for (int j = 1; j < n; ++j) {
     int cur = ((mask >> j) & 1) + ((mask >> (j - 1)) & 1);
     int sum = ((opp >> j) & 1) + ((opp >> (j - 1)) & 1);
     if (cur == 0) {
       if (sum == 0) {
-        fl = false;
-        break;
+        return false;
       }
       if (sum == 1) {
         opp += (1LL << j);
@@ -38,44 +38,41 @@ void MakeCorresponing(int n, int mask, int& opp, bool& fl) {
       if (sum == 0) {
         continue;
       }
-      fl = false;
-      break;
+      return false;
     }
   }
+  return true;
 }
 
 signed main() {
   int n, m;
   std::cin >> n >> m;
-  std::vector<std::string> s(n);
+  std::vector<std::string> map(n);
   for (int i = 0; i < n; ++i) {
-    std::cin >> s[i];
+    std::cin >> map[i];
   }
-  std::vector<std::vector<int>> dp(m, std::vector<int>(1LL << n, -1));
+  std::vector<std::vector<int>> dp(m, std::vector<int>(1LL << n, kUnused));
+  //  dp[i][j] - количество различных вариантов распределения районов города на
+  //  безоружные и излишне защищенные на префиксе карты района размера i,
+  //  если в i-том столбце распределние районов соответствует битовой маске j
   for (int mask = 0; mask < (1LL << n); ++mask) {
-    if (Check(n, mask, 0, s)) {
+    if (Check(n, mask, 0, map)) {
       dp[0][mask] = 1;
     }
   }
   for (int i = 1; i < m; ++i) {
     for (int mask = 0; mask < (1LL << n); ++mask) {
-      if (Check(n, mask, i, s)) {
+      if (Check(n, mask, i, map)) {
         int res = 0;
 
         int opp = 0;
-        bool fl = true;
-        MakeCorresponing(n, mask, opp, fl);
-
-        if (fl && dp[i - 1][opp] != -1) {
+        if (IsThereCorresponing(n, mask, opp) && dp[i - 1][opp] != kUnused) {
           res += dp[i - 1][opp];
           res %= kMod;
         }
 
         opp = 1;
-        fl = true;
-        MakeCorresponing(n, mask, opp, fl);
-
-        if (fl && dp[i - 1][opp] != -1) {
+        if (IsThereCorresponing(n, mask, opp) && dp[i - 1][opp] != kUnused) {
           res += dp[i - 1][opp];
           res %= kMod;
         }
@@ -88,7 +85,7 @@ signed main() {
   }
   int ans = 0;
   for (int i = 0; i < (1LL << n); ++i) {
-    if (dp[m - 1][i] != -1) {
+    if (dp[m - 1][i] != kUnused) {
       ans += dp[m - 1][i];
       ans %= kMod;
     }
