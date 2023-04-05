bccef171a631cbce68e8ebf641686e97d392ffd9
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2023-04-06 00:47:25 +0300

    commit taskE contest7 v2
    https://contest.yandex.ru/contest/47173/run-report/85230780/

--- 7contest/taskE.cpp
+++ 7contest/taskE.cpp
@@ -1,66 +1,54 @@
 #include <algorithm>
 #include <iostream>
-#include <map>
-#include <set>
-#include <string>
 #include <vector>
+
+struct Edge {
+  int to, num;
+};
 
 class Graph {
  private:
-    struct Edge {
-      int to, num;
-    };
-
-    int n, m;
-    std::vector<std::vector<Edge>> gr;
-    std::vector<int> used, dp, hight, res;
+    int n_, m_;
+    std::vector<std::vector<Edge>> gr_;
+    std::vector<int> used_, dp_, height_, res_;
 
  public:
-    void Init() {
-      std::cin >> n >> m;
-      gr.resize(n);
-      used.resize(n);
-      dp.resize(n);
-      hight.resize(n);
-      for (int i = 0; i < m; ++i) {
-        int u, v;
-        std::cin >> u >> v;
-        --u, --v;
-        gr[u].push_back({v, i + 1});
-        gr[v].push_back({u, i + 1});
-      }
+    Graph(int n, int m, const std::vector<std::vector<Edge>>& gr): n_(n), m_(m), gr_(gr) {
+      used_.resize(n_);
+      dp_.resize(n_);
+      height_.resize(n_);
     }
 
     void FindBridges() {
-      for (int i = 0; i < n; ++i) {
-        if (used[i] == 0) {
+      for (int i = 0; i < n_; ++i) {
+        if (used_[i] == 0) {
           Dfs(i);
         }
       }
     }
     
     void Dfs(int v, Edge p = {-1, -1}) {
-      used[v] = 1;
-      dp[v] = hight[v] = (p.num == -1 ? 0 : hight[p.to] + 1);
-      for (auto ed : gr[v]) {
+      used_[v] = 1;
+      dp_[v] = height_[v] = (p.num == -1 ? 0 : height_[p.to] + 1);
+      for (auto ed : gr_[v]) {
         if (ed.num != p.num) {
-          if (used[ed.to] == 1) {
-            dp[v] = std::min(dp[v], hight[ed.to]);
+          if (used_[ed.to] == 1) {
+            dp_[v] = std::min(dp_[v], height_[ed.to]);
           } else {
             Dfs(ed.to, {v, ed.num});
-            dp[v] = std::min(dp[v], dp[ed.to]);
-            if (hight[v] < dp[ed.to]) {
-              res.push_back(ed.num);
+            dp_[v] = std::min(dp_[v], dp_[ed.to]);
+            if (height_[v] < dp_[ed.to]) {
+              res_.push_back(ed.num);
             }
           }
         }
       }
     }
 
-    void GetRes() {
-      std::cout << res.size() << "\n";
-      sort(res.begin(), res.end());
-      for (auto x : res) {
+    void PrintAllBridges() {
+      std::cout << res_.size() << "\n";
+      sort(res_.begin(), res_.end());
+      for (auto x : res_) {
         std::cout << x << " ";
       }
     }
@@ -68,9 +56,18 @@ class Graph {
 
 
 signed main() {
-  Graph MyGraph;
-  MyGraph.Init();
-  MyGraph.FindBridges();
-  MyGraph.GetRes();
+  int n, m;
+  std::cin >> n >> m;
+  std::vector<std::vector<Edge>> gr(n);
+  for (int i = 0; i < m; ++i) {
+    int u, v;
+    std::cin >> u >> v;
+    --u, --v;
+    gr[u].push_back({v, i + 1});
+    gr[v].push_back({u, i + 1});
+  }
+  Graph my_graph(n, m, gr);
+  my_graph.FindBridges();
+  my_graph.PrintAllBridges();
   return 0;
 }
