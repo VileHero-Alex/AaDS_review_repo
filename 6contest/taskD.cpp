9e9c711a63ba8dfd489af0f50f2ff2c3a1965722
Aleksei Liulko <46903183+VileHero-Alex@users.noreply.github.com>
2023-04-06 15:25:43 +0300

    commit taskD contest6 v2
    https://contest.yandex.ru/contest/46074/run-report/85254263/

--- 6contest/taskD.cpp
+++ 6contest/taskD.cpp
@@ -1,49 +1,28 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
 #include <vector>
 
-void SpeedUp() {
-  std::ios_base::sync_with_stdio(false);
-  std::cin.tie(0);
-  std::cout.tie(0);
-}
-
-int dp[1000][100000] = {{0}};
-
-int GetRes(int k, int res) {
-  if (k > res) {
-    return GetRes(res, res);
-  }
-  if (k == 0 || res == 0) {
-    return 0;
-  }
-  if (k == 1) {
-    return res;
-  }
-  if (dp[k][res] == 0) {
-    dp[k][res] = GetRes(k, res - 1) + GetRes(k - 1, res - 1) + 1;
-  }
-  return dp[k][res];
-}
-
-signed main() {
-  SpeedUp();
-  int n, k;
-  std::cin >> n >> k;
-
-  if (n == 1) {
-    std::cout << "0\n";
-    return 0;
-  }
-
+int Solve(int n, int k) {
   if (k == 0) {
-    std::cout << "-1\n";
-    return 0;
+    if (n == 1) {
+      return 0;
+    }
+    return -1;
   }
-  int res = 1;
-  while (GetRes(k, res) < n - 1) {
+  std::vector<int> dp(k + 1, 0);
+  // количество яиц, которые нужно бросить, чтобы
+  // определить этаж, если номер этажа равен i
+  int res = 0;
+  while (dp[k] < n - 1) {
+    for (int cur = k; cur > 0; --cur) {
+      dp[cur] += dp[cur - 1] + 1;
+    }
     ++res;
   }
-  std::cout << res << "\n";
+  return res;
+}
+
+int main() {
+  int n, k;
+  std::cin >> n >> k;
+  std::cout << Solve(n, k);
 }
